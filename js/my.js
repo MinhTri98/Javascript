8241fa502bab767b6fcfc0c27d26c86277bf0f1c
NguyenMinhTri <minhtringuyen0498@gmail.com>
2019-05-11 09:14:53 +0700

    vom

--- js/my.js
+++ js/my.js
@@ -1,29 +1,43 @@
-// //câu 1:
-let a,b;
-function min(a,b){
-   let message = (a>b) ? b: (a<b) ? a :  a||b;
-   alert (message);
-}//hàm kiểm tra bằng mutiple '?'
-min(3,2);
+//câu 1:
+// let a, b;
+// function min(a, b) {
+//    let message = (a > b) ? b: (a < b) ? a :  a||b;
+//    alert (message);
+// }//hàm kiểm tra bằng mutiple '?'
+// min(3,2);
 
-function min2(a,b){
-   if(a>b){
-      alert(b);
-   }
-   else if(a<b){
-      alert(a);
-   }
-   else{
-      alert(a||b);
-   }
-}//hàm kiểm tra bằng if else
-min2(3,5);
+// function min2(a,b){
+//    if(a>b){
+//       alert(b);
+//    }
+//    else if(a<b){
+//       alert(a);
+//    }
+//    else{
+//       alert(a||b);
+//    }
+// }//hàm kiểm tra bằng if else
+// min2(3,5);
 
-//câu 2:
-let x,n
-function pow(x,n){
-   alert(x**n);
+// //câu 2:
+// let x,n
+// function pow(x,n){
+//    alert(x**n);
+// }
+// let input = prompt('Mời bạn nhập x: ',x);
+// let input2 = prompt('Mời bạn nhập n: ',n);
+// pow(input,input2);
+
+//cau 1
+// let ask = (question,yes,no) => {
+//    if( confirm(question)) yes();
+//    else no();
+// }
+// ask("Do you agree",() => alert("You agreed"), () => alert("You canceled the execution"));
+
+//cau 2
+let i=0;
+while (i<3){
+   alert( `number ${i}!` );
+   i++;
 }
-let input = prompt('Mời bạn nhập x: ',x);
-let input2 = prompt('Mời bạn nhập n: ',n);
-pow(input,input2);
