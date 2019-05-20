945feb9bb923679542a60af01c9bf308b1c803b6
NguyenMinhTri <minhtringuyen0498@gmail.com>
2019-05-20 22:33:26 +0700

    js

--- js/my.js
+++ js/my.js
@@ -6,22 +6,22 @@
 // }//hàm kiểm tra bằng mutiple '?'
 // min(3,2);
 
-// function min2(a,b){
-//    if(a>b){
+// function min2(a,b) {
+//    if(a>b) {
 //       alert(b);
 //    }
-//    else if(a<b){
+//    else if(a<b) {
 //       alert(a);
 //    }
-//    else{
+//    else {
 //       alert(a||b);
 //    }
 // }//hàm kiểm tra bằng if else
 // min2(3,5);
 
 // //câu 2:
-// let x,n
-// function pow(x,n){
+// let x,n;
+// function pow(x,n) {
 //    alert(x**n);
 // }
 // let input = prompt('Mời bạn nhập x: ',x);
@@ -37,56 +37,104 @@
 
 //cau 2
 // let i=0;
-// while (i<3){
+// while (i<3) {
 //    alert( `number ${i}!` );
 //    i++;
 // }
 
 //object
 
-let user = {
-   name : "John",
-   surname : "Smith"
+// let user = {
+//    name : "John",
+//    surname : "Smith"
+// };
+// alert(user.name);
+
+// user.name = "Pete";
+// alert(user.name);
+
+// delete user.name;
+// alert(user.name);
+
+// let salaries = {
+//    John : 100,
+//    Ann : 160,
+//    Pete : 130
+// };
+// let result = 0;
+
+// if ("John" in salaries && "Ann" in salaries && "Pete" in salaries) {
+//   let sum = 0; 
+//   for(let price in salaries) {
+//       sum = sum + salaries[price];
+//   }
+//   result = sum;
+// }
+// else{
+//    result = 0;
+// }
+
+// alert(result);
+
+// let menu = {
+//    width: 200,
+//    height: 300,
+//    title: "My menu"
+//  };
+
+// function multiplyNumeric(menu) {
+//     for (let key in menu){
+//       if  (typeof(menu[key]) == 'number') {
+//         menu[key] = menu[key] * 2;
+//       }
+//       console.log(menu[key]);
+//     }
+// }
+
+// multiplyNumeric(menu);
+
+//Array
+let nhanVien1 = {
+  name : "tri",
+  luong : 2000
 };
-alert(user.name);
 
-user.name = "Pete";
-alert(user.name);
-
-delete user.name;
-alert(user.name);
-
-let salaries = {
-   John : 100,
-   Ann : 160,
-   Pete : 130
+let nhanVien2 = {
+  name : "duy",
+  luong : 1000
 };
-let result = 0;
-if ("John" in salaries && "Ann" in salaries && "Pete" in salaries){
-   let sum = 0;
-   for(let price in salaries){
-      sum = sum + salaries[price];
-   }
-   result = sum;
-}
-else{
-   result = 0;
+
+let nhanVien3 = {
+  name : "Vien",
+  luong : 10000
+};
+
+let arr = [];
+
+function addNhanVien(obj, obj1, obj2){
+  arr.push(obj);
+  arr.push(obj1);
+  arr.push(obj2);
 }
 
-alert(result);
+// function tongLuong (arr) {
+//   let tong = 0;
+//   for(let i = 0 ; i < arr.length ; i++){
+//     tong = tong + arr[i].luong;
+//   }
+//   console.log(tong);
+// }
 
-let menu = {
-   width: 200,
-   height: 300,
-   title: "My menu"
- };
+addNhanVien(nhanVien1, nhanVien2, nhanVien3);
+console.log(arr);
 
-function multiplyNumeric(menu){
-    for (let key in menu){
-      if  (typeof(menu[key]) == 'number'){
-        menu[key] = menu[key]*2;
-      }
-      alert(menu[key]);
-    }
+// tongLuong(arr);
+
+function deleteNhanVien (arr) {
+  let viTri;
+  let viTri1 = prompt("Bạn muốn xóa nhân viên thứ mấy",viTri);
+  arr.splice(viTri1,1);
 }
-multiplyNumeric(menu);
+deleteNhanVien(arr);
+
+console.log(arr);
